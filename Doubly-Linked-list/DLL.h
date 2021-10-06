7bb717a0d8a998c9f7376472e2bd31ee821de995
SabiyaNaseem <88092686+SabiyaNaseem@users.noreply.github.com>
2021-10-06 14:53:55 +0530

    Add files via upload

---
+++ Doubly-Linked-list/DLL.h
@@ -0,0 +1,18 @@
+
+#ifndef DOUBLY_LINKED_LIST
+#define DOUBLY_LINKED_LIST
+
+typedef int data;
+typedef struct double_node{
+    data value;
+  struct double_node *prev;
+   struct double_node *next;
+   }DoubleNode;
+   DoubleNode* dll_create_node(data value);
+   DoubleNode* dll_prepend(DoubleNode* head,data value);
+   DoubleNode* dll_insert_before(DoubleNode* node,data value);
+   DoubleNode* dll_insert_after(DoubleNode* node,data value);
+   DoubleNode* dll_delete_node(DoubleNode *head,DoubleNode *node);
+   DoubleNode* dll_find(DoubleNode* head,data value);
+
+   #endif
